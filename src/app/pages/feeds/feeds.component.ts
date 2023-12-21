a05841da32046414ddd326a91dac2e0dec6b81e0
Aman Yadav <amanyddv@gmail.com>
2023-12-21 19:01:01 +0530

    loading option added

--- src/app/pages/feeds/feeds.component.ts
+++ src/app/pages/feeds/feeds.component.ts
@@ -9,11 +9,17 @@ export class FeedsComponent {
   constructor(private service:ServiceService){}
 
   newsletterItems:any
+  loading = true;
+
   ngOnInit(): void {
     this.service.newsletterfeeds().subscribe((res)=>{
       this.newsletterItems=res
     
     })
+
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000); 
   }
 
   
