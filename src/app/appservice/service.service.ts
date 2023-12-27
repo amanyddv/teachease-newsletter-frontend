61aefcb95a135b9fe9b3f89b9db718199b9bd71e
Aman Yadav <amanyddv@gmail.com>
2023-12-28 00:34:14 +0530

    add env

--- src/app/appservice/service.service.ts
+++ src/app/appservice/service.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  apiUrl = environment.apiUrl; // Use environment.apiUrl here
 
   constructor(private http:HttpClient) { }
 
-  ur="http://localhost:7000/";
-  url="https://techease-newsletter-api.vercel.app/";
+  
+  
+  url=`${this.apiUrl}`;
 
   postnewsletter(data:any){
     this.http.post<any>(this.url+"postnewsletter",data).subscribe((res)=>{
